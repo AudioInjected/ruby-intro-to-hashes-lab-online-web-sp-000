3a4b0faf14bda54c77ccb4355af29288ceea3d57
Rafael Batista <rafaelb24@yahoo.com>
2018-10-19 19:31:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,22 @@ def monopoly_with_second_tier
 			pieces: 4,
 			names:
 			{
+				reading_railroad:
+				{
 
+				},
+				pennsylvania_railroad:
+				{
+
+				},
+				b_and_o_railroad:
+				{
+
+				},
+				shortline:
+				{
+
+				}
 			},
 			rent_in_dollars:
 			{
